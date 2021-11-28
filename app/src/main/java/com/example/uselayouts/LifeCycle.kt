9bfbee1a04fdf82b309e1576278df4efc52b44e6
OliviaYJH <0703olivia@naver.com>
2021-11-29 03:21:41 +0900

    add: LifeCycle

---
+++ app/src/main/java/com/example/uselayouts/LifeCycle.kt
@@ -0,0 +1,39 @@
+package com.example.uselayouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Toast
+
+class LifeCycle : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_life_cycle)
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT)
+    }
+}
